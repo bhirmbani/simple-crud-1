1fd231bccae2e2da3fb4fd3058f91af7badbab37
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-19 13:56:43 +0700

    adding partials files,fixing association setup in movie and movier,show the list of movie in index.ejs

--- models/movier.js
+++ models/movier.js
@@ -8,7 +8,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Movier.belongsToMany(models.Movie, {through: MovierMovie, foreignKey: 'movier_id'});
+        Movier.belongsToMany(models.Movie, {through: models.MovierMovie, foreignKey: 'movier_id'});
       }
     }
   });
